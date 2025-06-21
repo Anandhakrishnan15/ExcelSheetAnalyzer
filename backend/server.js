d1c9e4cc6eff43da32c533fc4b4c81af65e59f3d
GirishEgo <Jai.girishego@gmail.com>
2025-06-21 19:07:05 +0530

    server

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routers/authRoutes');
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(helmet());
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
