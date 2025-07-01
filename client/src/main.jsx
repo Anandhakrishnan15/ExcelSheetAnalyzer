573281f54f1b54e037f0b2adeb3c9e4ae7dbbebc
GirishEgo <Jai.girishego@gmail.com>
2025-07-01 17:12:04 +0530

    adde the ExelUploadProvider to the APP

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { ExcelUploadProvider } from './context/excelUploadcontext.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ExcelUploadProvider>
+          <App />
+        </ExcelUploadProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
