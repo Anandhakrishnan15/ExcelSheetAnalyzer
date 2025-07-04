defaf8235207e72d0f461f26337cfff181d9e4a5
GirishEgo <Jai.girishego@gmail.com>
2025-07-04 14:15:41 +0530

    sibel mobive respovie

--- client/src/components/ChartUploads/Sidebar.jsx
+++ client/src/components/ChartUploads/Sidebar.jsx
@@ -2,18 +2,21 @@ import SidebarToggleButton from "./SidebarToggleButton";
 import DataTable from "../DataTable";
 import AllUploedExels from "./AllUploedExels";
 
-const Sidebar = ({ isOpen, toggle, filename, fileData }) => (
+const Sidebar = ({ isOpen, toggle, filename, fileData, isMobile }) => (
   <aside
-    className={`relative transition-all duration-700 ease-in-out
-      ${isOpen ? "w-120 p-4 scrollbar-black" : "w-10 p-1"}
-      bg-[var(--body)] border-r`}
+    className={`sticky top-0 left-0 max-h-[calc(130vh-5rem)] bg-[var(--body)] border-r
+    transition-all duration-700 ease-in-out
+    ${isOpen ? (isMobile ? "w-90 p-4" : "w-120 p-4") : "w-10 p-1"}
+  `}
   >
     <SidebarToggleButton isOpen={isOpen} onToggle={toggle} />
+
     {isOpen && (
-      <div className="space-y-4 animate-fadeIn">
+      <div className=" overflow-y-auto overflow-x-hidden max-h-[calc(120vh-5rem)] space-y-4 pr-2 scrollbar-none">
         <h3 className="text-lg font-semibold">Chart Types</h3>
-        <DataTable filename={filename} rows={fileData?.rows} />
+
         <AllUploedExels />
+        <DataTable filename={filename} rows={fileData?.rows} />
       </div>
     )}
   </aside>
