5a462b7958fa1acdc6d067c92d78e55f6aa1a12b
GirishEgo <Jai.girishego@gmail.com>
2025-07-07 16:32:14 +0530

    file data Provider added to the main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,18 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { ExcelUploadProvider } from './context/excelUploadcontext.jsx';
+import { FileDataProvider } from './context/FileDataContext.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <ExcelUploadProvider>
-          <App />
-        </ExcelUploadProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <FileDataProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <ExcelUploadProvider>
+            <App />
+          </ExcelUploadProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </FileDataProvider>
   </StrictMode>
 );
