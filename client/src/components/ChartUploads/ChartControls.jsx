f6c9f54e3711890640c2d0125dc4d07b6d725cdb
GirishEgo <Jai.girishego@gmail.com>
2025-07-11 17:52:49 +0530

    dropn down for y axis with chaek box

--- client/src/components/ChartUploads/ChartControls.jsx
+++ client/src/components/ChartUploads/ChartControls.jsx
@@ -1,3 +1,5 @@
+import MultiSelectDropdown from "../MultiSelectDropdown";
+
 const ChartControls = ({ chart, index, columns, updateChart, isMobile }) => {
   const selectClass = `
     border border-[var(--border)] 
@@ -10,16 +12,20 @@ const ChartControls = ({ chart, index, columns, updateChart, isMobile }) => {
     ${isMobile ? "w-15" : "w-auto"}
   `;
 
+  const yAxisSelected = Array.isArray(chart.yAxis)
+    ? chart.yAxis
+    : [chart.yAxis].filter(Boolean);
+
   return (
-    <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-4 w-full mb-4">
+    <div className="flex flex-wrap md:flex-nowrap items-start justify-between gap-4 w-full mb-4">
       {/* Axis selectors */}
-      <div className="flex gap-3 flex-wrap">
+      <div className="flex gap-6 flex-wrap">
         {/* X Axis */}
         <div className="flex flex-col">
           <label className="text-xs font-medium mb-1">X-Axis</label>
           <select
             className={selectClass}
-            value={chart.xAxis}
+            value={chart.xAxis || ""}
             onChange={(e) => updateChart(index, "xAxis", e.target.value)}
           >
             <option className="bg-[var(--border)]" value="">
@@ -35,35 +41,32 @@ const ChartControls = ({ chart, index, columns, updateChart, isMobile }) => {
 
         {/* Y Axis */}
         <div className="flex flex-col">
-          <label className="text-xs font-medium mb-1">Y-Axis</label>
-          <select
-            className={selectClass}
-            value={chart.yAxis}
-            onChange={(e) => updateChart(index, "yAxis", e.target.value)}
-          >
-            <option className="bg-[var(--border)]" value="">
-              Select column
-            </option>
-            {columns.map((col) => (
-              <option key={col} value={col}>
-                {col}
-              </option>
-            ))}
-          </select>
+          <label className="text-xs font-medium mb-1">Y-Axis (multiple)</label>
+          <MultiSelectDropdown
+            options={columns}
+            selected={yAxisSelected}
+            onChange={(newSelection) =>
+              updateChart(index, "yAxis", newSelection)
+            }
+            placeholder="Select columns"
+          />
         </div>
       </div>
 
-      {/* Graph Type */}
+      {/* Chart Type */}
       <div className="flex flex-col">
-        <label className="text-xs font-medium mb-1">Graph Type</label>
+        <label className="text-xs font-medium mb-1">Chart Type</label>
         <select
           className={selectClass}
-          value={chart.graphType || "bar"}
-          onChange={(e) => updateChart(index, "graphType", e.target.value)}
+          value={chart.type || "bar"}
+          onChange={(e) => updateChart(index, "type", e.target.value)}
         >
           <option value="bar">Bar</option>
           <option value="line">Line</option>
+          <option value="area">Area</option>
+          <option value="scatter">Scatter</option>
           <option value="pie">Pie</option>
+          <option value="doughnut">Doughnut</option>
         </select>
       </div>
     </div>
