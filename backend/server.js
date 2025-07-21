b461ee5a24fffaf62bdfc4ba54c54e8792ba9f07
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2025-07-21 20:03:55 +0530

    handel SPA routers

--- backend/server.js
+++ backend/server.js
@@ -53,9 +53,9 @@ app.use("/api", alldashboardRoutes);
 // Serve React frontend (Vite build)
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 app.get("/", (req, res) => {
     res.send("Backend is running 🚀");
 });
